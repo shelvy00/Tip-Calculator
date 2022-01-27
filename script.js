0ef17abc0f4fc2374c104e98f88ab8123ab41436
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2022-01-26 21:18:09 -0600

    update.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  function calculateTip() {
  	const billAmount = document.getElementById("bill-amount").value;
  	const tipPercentage = document.getElementById("tip-percentage").value;
- 	const tipAmount = document.getElementById("tip-amount").value = billAmount / tipPercentage;
+ 	const tipAmount = document.getElementById("tip-amount").value = billAmount * (tipPercentage / 100);
  	document.getElementById("total-bill").value = parseFloat(billAmount) + parseFloat(tipAmount);
  	console.log(billAmount)
  }
